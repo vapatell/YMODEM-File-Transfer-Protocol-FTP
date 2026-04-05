0a1e72d1d9586eea2966ed56bea6949926399140
Viraj Patel <virajpatel175@gmail.com>
2026-04-04 19:49:55 -0700

    YMODEM FTP simulation using 128 byte blocks and CRC16 in Network Byte Order for little & big-endian system

---
+++ Part 1 - Linux VMWare, 16-bit CRC, 128 byte blocks/eclipse-workspace-2021-06/Ensc351Part1-1217/DebugMips/src/subdir.mk
@@ -0,0 +1,40 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../src/Ensc351Part1.cpp \
+../src/PeerY.cpp \
+../src/SenderY.cpp \
+../src/myIO.cpp 
+
+OBJS += \
+./src/Ensc351Part1.o \
+./src/PeerY.o \
+./src/SenderY.o \
+./src/myIO.o 
+
+CPP_DEPS += \
+./src/Ensc351Part1.d \
+./src/PeerY.d \
+./src/SenderY.d \
+./src/myIO.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.cpp src/subdir.mk
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross G++ Compiler'
+	mips-linux-gnu-g++ -std=c++17 -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+src/PeerY.o: ../src/PeerY.cpp src/subdir.mk
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross G++ Compiler'
+	mips-linux-gnu-g++ -std=c++17 -O0 -g3 -Wall -c -fmessage-length=0 -Wno-register -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
